7ab87a9eeca6d626e550e69488a2ac20ce89876e
shockylemi <shockylemi@gmail.com>
2022-07-02 09:07:52 +0430

    test header

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use('/', express.static(path.join(__dirname, '/public')))
 
 // default page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public' , 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 // test post endpoint
